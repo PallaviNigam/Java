a50f4ab35525e51fee8731211a4c1eadac7d30d8
PallaviNigam <pallavinigam2014@gmail.com>
2019-02-07 23:39:21 +0530

    Create WrapperClassAutoboxingInboxing.java

---
+++ WrapperClassAutoboxingInboxing.java
@@ -0,0 +1,14 @@
+//Help developers to write cleaner code 
+public class Test
+{
+    public static void main(String[] args)
+    {
+        Integer i = 5;
+        //Autoboxing 
+        System.out.println(i);
+        //Inboxing
+        int j = i;
+        
+         System.out.println(j);
+    }
+}
