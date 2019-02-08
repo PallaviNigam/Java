6ae53c491e8a22c2439b18581125c622096a0344
PallaviNigam <pallavinigam2014@gmail.com>
2019-02-08 23:50:01 +0530

    Create WorkingwithmainThread.java

---
+++ WorkingwithmainThread.java
@@ -0,0 +1,18 @@
+public class HelloWorld extends ExceptionTest
+{
+public static void main(String[] agrgs)
+{
+	
+System.out.println("hii");
+try
+{
+Thread.sleep(500);
+}
+catch(Exception e)
+{
+	
+	} 
+System.out.println("hello");
+}
+
+}
